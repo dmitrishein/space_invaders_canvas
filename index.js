4463c4eff2e765549414596aad50f3676ea7ba62
dmitrishein <drghost9899@gmail.com>
2023-06-06 22:02:34 +0300

    base game functionality

---
+++ index.js
@@ -0,0 +1,260 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d'); 
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.rotation = 0;
+
+        const image = new Image();
+        image.src = './img/spaceship.png';
+
+        image.onload = () => {
+            const scale = 0.40;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height  - this.height - 20
+            }
+    
+        };
+    }
+
+    draw() {
+        context.save();
+
+        context.translate(
+            player.position.x + player.width / 2, 
+            player.position.y + player.height / 2
+        );
+        context.rotate(this.rotation);
+
+        context.translate(
+            -player.position.x - player.width / 2, 
+            -player.position.y - player.height / 2
+        );
+
+        context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+
+        context.restore();
+        
+    }
+
+    update() {
+        if (this.image) {
+            this.draw();
+            this.position.x += this.velocity.x;
+        }
+    }
+}
+class Projectile {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.radius = 3;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        context.fillStyle = 'red';
+        context.fill();
+        context.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y; 
+    }
+}
+class Invader {
+    constructor({ position }) {
+        const image = new Image();
+        image.src = './img/invader.png';
+
+        image.onload = () => {
+            const scale = 2;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+
+            this.position = {
+                x: position.x,
+                y: position.y
+            }
+    
+        };
+    }
+
+    draw() {
+        context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);        
+    }
+
+    update({velocity}) {
+        if (this.image) {
+            this.draw();
+            this.position.x += velocity.x;
+            this.position.y += velocity.y;
+        }
+    }
+}
+class Grid {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 3, 
+            y: 0
+        }
+
+        this.invaders = [];
+
+        const rows = Math.floor(Math.random() * 5 + 5);
+        const columns = Math.floor(Math.random() * 10 + 2);
+
+        this.width = columns * 60;
+
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                this.invaders.push(new Invader({
+                    position : {
+                        x: i * 60,
+                        y: j * 60
+                        }
+                    })
+                );
+            }
+        }
+    }
+
+    update() {
+       this.position.x += this.velocity.x;
+       this.position.y += this.velocity.y;
+
+       this.velocity.y = 0;
+
+       if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+        this.velocity.x = -this.velocity.x;
+        this.velocity.y = 30;
+       }
+
+    }
+}
+
+
+const player = new Player();
+const projectiles = [];
+const grids = [new Grid()];
+
+const keys = {
+    a: {
+        isPressed: false
+    },
+    d: {
+        isPressed: false
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0) {
+            setTimeout(()=> {
+                projectiles.splice(index, 1);
+            });
+        }
+        projectile.update();
+    });
+
+    grids.forEach(grid => {
+        grid.update();
+        grid.invaders.forEach(invader => {
+            invader.update({velocity: grid.velocity});
+        });
+    });
+    if (keys.a.isPressed && player.position.x >= 0) {
+        player.velocity.x = -10;
+        player.rotation = -0.15;
+    } else if (keys.d.isPressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 10;
+        player.rotation = 0.15;
+    } else {
+        player.velocity.x = 0;
+        player.rotation = 0;
+    }
+}
+
+animate();
+
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'w': {
+            break;
+        }
+        case 'a': {
+            keys.a.isPressed = true;
+            break;
+        }
+        case 's': {
+            break;
+        }
+        case 'd': {
+            keys.d.isPressed = true;
+            break;
+        }
+        case ' ': {
+            projectiles.push(
+                new Projectile ({
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -27
+                    }
+                })
+            )
+            break;
+        }
+    }
+});
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'w': {
+            break;
+        }
+        case 'a': {
+            keys.a.isPressed = false;
+            break;
+        }
+        case 's': {
+            break;
+        }
+        case 'd': {
+            keys.d.isPressed = false;
+            break;
+        }
+        case ' ': {
+            break;
+        }
+    }
+});
